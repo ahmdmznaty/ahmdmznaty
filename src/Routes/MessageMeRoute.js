0b8af25e69ff790908682b4166bd37a97ce12a3f
Ahmed M. Znaty <ahmdmznaty@gmail.com>
2023-04-04 17:22:20 +0200

    Handle Heroku Errors

--- src/Routes/MessageMeRoute.js
+++ src/Routes/MessageMeRoute.js
@@ -28,20 +28,20 @@ export default function messageMe(req, res) {
                 html: `<div style="background: #eee; padding: 30px; border: 1px solid #ddd; border-radius: 5px; text-align: center; font-family: sans-serif; box-sizing: border-box;"><h3 style="width: 100%; margin: 0; color: #333; font-size: 30px;">${jsondata.name}</h3><p style="width: calc(100% - 10px); color: #666; margin: 15px auto; font-size: 16px; background: #fff; padding: 20px; border-radius: 5px; border: 1px solid #ddd; box-sizing: border-box;">${jsondata.message}</p><a style="margin: 5px; display: inline-block; text-decoration: none; width: 120px; height: 40px; background: #181817; color: #eee; line-height: 40px; border-radius: 4px;" href="mailto:${jsondata.email}">Email</a><a style="margin: 5px; display: inline-block; text-decoration: none; width: 120px; height: 40px; background: #181817; color: #eee; line-height: 40px; border-radius: 4px;" href="https://wa.me/+${jsondata.whatsapp.replaceAll("+", "")}">Whatsapp</a></div>`
             };
             
-            fs.readFile("./messages.json", "utf8", (err, data) => {
-                data = JSON.parse(data)
-                if(data.some(message => (message.name == jsondata.name && (message.email == jsondata.email || message.whatsapp == jsondata.whatsapp)))) {
-                    res.statusCode = 500
-                    res.end("error")
-                }
-                else {
+            // fs.readFile("./messages.json", "utf8", (err, data) => {
+                // data = JSON.parse(data)
+                // if(data.some(message => (message.name == jsondata.name && (message.email == jsondata.email || message.whatsapp == jsondata.whatsapp)))) {
+                //     res.statusCode = 500
+                //     res.end("error")
+                // }
+                // else {
                     transporter.sendMail(mailOptions, function(error, info){
                         if (error) {
                             res.statusCode = 500
-                            res.end(error)
+                            res.end("error")
                         } else {
-                            data.push(jsondata)
-                            fs.writeFile("./messages.json", JSON.stringify(data), error => {
+                            // data.push(jsondata)
+                            // fs.writeFile("./messages.json", JSON.stringify(data), error => {
                                 if(error) {
                                     res.statusCode = 500
                                     res.end("error")
@@ -50,11 +50,11 @@ export default function messageMe(req, res) {
                                     res.statusCode = 200
                                     res.end("done")
                                 }
-                            })
+                            // })
                         }
                     });
-                }
-            })
+                // }
+            // })
         })
     }
 }
